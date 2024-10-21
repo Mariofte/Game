f7b925a998fe216b8b861ca6b88121a00b7c95e7
Mariofte <150403921+Mariofte@users.noreply.github.com>
2024-10-21 00:16:42 -0600

    ya mejore la ventana

--- main.py
+++ main.py
@@ -1,42 +1,52 @@
 import pygame 
 import sys
+import os 
 
 def main ():
+    # posicionar la vaentana en la pantalla
+    os.environ['SDL_VIDEO_WINDOW_POS'] = "300,100"
     
     # Inicializar pygame
     pygame.init()
 
     # Tamaño de la ventana
-    tam = (800,600)
+    tam = (800,800)
     screen = pygame.display.set_mode(size = tam)
+    
+    # fps
+    #clock = pygame.time.Clock()
 
     # Nombre de la ventana
     pygame.display.set_caption(title="El perdido")
     
-    runnig = True
-    while runnig:
+    running = True
+    while running:
         try:
             
             
             # Revisar los eventos
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: # Si el gugador cierra la vantana
-                    runnig = False
+                    running = False
             
             # Actualizar la pantalla
             pygame.display.flip()
             
+            # clock.tick(framerate=60)
+            
         except ModuleNotFoundError as error:
             print(f"Error de líbrerias:{error}")
-            
+            running = False
+        
         except pygame.error as error:
             print(f"Error en el codígo: {error}")
-            runnig = False
+            running = False
         
         except Exception as error:
             print(f"Error desconocido:{error}")
+            running = False
             
-    # Salir de pygama
+    # Salir de pygame
     pygame.quit()
     sys.exit()
     
